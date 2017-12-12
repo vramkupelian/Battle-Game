23b641ded5bfc51fff524797f7379a3fe6fc5ac5
unknown <vramkupelian@gmail.com>
2017-12-12 01:53:22 -0800

    added attack button: non-functional added background image adjust borders and colors to match boxing theme added attackPower and labelNumber values from array to each imageFighter.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,90 @@
 var healthBar = [100, 200, 300, 400];
+var attackPower = [200,175,150,125];
+var labelNumber = [0,1,2,3];
+
+var damageCounter =0;
+
 
 for(var i =0; i < healthBar.length; i++){
-    var divForImage = $("<div>");
+    var hasBeenClicked = false;
+    // var divForImage = $("<div>");
+    
+    //For each iteration, create an imageFighter
     var imageFighter = $("<img>");
-    var healthBarSpan = $ ("<span>");
-
-    divForImage.addClass("div-for-image");
+    
+    // var healthBarSpan = $ ("<span>");
+    // divForImage.addClass("div-for-image");
+    
+    //give each imageFighter the class "fighter", css
     imageFighter.addClass("fighter");
-    healthBarSpan.addClass("health-bar-span");
+    
+    // imageFighter.val(i);  ??
+    
+    // healthBarSpan.addClass("health-bar-span");
 
     // Each imageFighter will be given a src link to the image
     imageFighter.attr("src", "./assets/images/fighter" + i + ".jpg");
  
-    //Each fighter will get a healthbar based off of position 
-    healthBarSpan.attr("data-healthBar", healthBar[i]); 
+    //Each fighter will get a data attribute "data-healthBar", set = healthBar array. etc.
+    imageFighter.attr("data-healthBar", healthBar[i]);
+    imageFighter.attr("data-labelNumber", labelNumber[i]);
+    imageFighter.attr("data-attackPower", attackPower[i]);
+    //  Each gets data boolean 
+    imageFighter.attr("data-hasBeenClicked", hasBeenClicked);
 
+    labelNumber = parseInt(labelNumber);
+
+    //add to page
     $(".container-pick").append(imageFighter);
-    $(".container-pick").append(healthBarSpan);
+    // $(".fighter").append(healthBarSpan);
 }
+//----------------------------------close for loop------
+
+
+$(function sortIfPickedBlue(){
+
+
+    //check if something has been clicked.
+
+
+    if (hasBeenClicked) {
+
+
+        
+        if(!hasBeenClicked){
+            $(".container-rest").append(imageFighter);
+        }
+    } 
+    
+})
+
+
+//------start on click events-----
+
+// //when you click on image
+$(".fighter").on("click", function() {
+
+    //this for this clicked fighter
+    var damageValue = ($(this).attr("data-attackPower"));
+   
+    //parses string (html attribute) into integer
+    damageValue = parseInt(damageValue);
+
+    // every click adds to global counter. will need to be changed
+    damageCounter += damageValue;
+
+    
+    var hasBeenClicked = true;
+
+
+    // run a function  that sorts after someone is picked.
+    $(this).addClass("my-pick");
+    console.log("you clicked a fighter. Counter =" + damageCounter);
+    
+})
+
+
+
+//this moves from one element to another. 
+// // $(target_element).append(to_be_inserted_element)
 
