8bd7af26c8296b86ab04d1836fc2c13687e92717
unknown <vramkupelian@gmail.com>
2017-12-11 21:46:49 -0800

    Added images of fighters and assign each one healthBar value.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,21 @@
 var healthBar = [100, 200, 300, 400];
 
 for(var i =0; i < healthBar.length; i++){
+    var divForImage = $("<div>");
+    var imageFighter = $("<img>");
+    var healthBarSpan = $ ("<span>");
 
-var imageFighter = $("<img>");
-var healthBarSpan = $ ("<span>");
+    divForImage.addClass("div-for-image");
+    imageFighter.addClass("fighter");
+    healthBarSpan.addClass("health-bar-span");
 
-imageFighter.addClass("fighter");
-
- // Each imageCrystal will be given a src link to the crystal image
- imageFighter.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
+    // Each imageFighter will be given a src link to the image
+    imageFighter.attr("src", "./assets/images/fighter" + i + ".jpg");
  
+    //Each fighter will get a healthbar based off of position 
+    healthBarSpan.attr("data-healthBar", healthBar[i]); 
 
-imageFighter.attr("data-healthBar", healthBar[i]); 
-
-$(".container-pick").append(imageFighter);
-$(".health-bar-span").append()
+    $(".container-pick").append(imageFighter);
+    $(".container-pick").append(healthBarSpan);
 }
 
